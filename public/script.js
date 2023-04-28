0227302fda05fa03d0dfada355c60e3bd5d4f837
jamey-albert <albertj6@wwu.edu>
2023-04-28 15:25:24 -0700

    Add functions to read from text file and render board on page.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,63 @@
-console.log("hello,world!");
+window.onload = () => {
+  let board = getBoard(); // load right away
+};
+
+// Read text from given filename and parse the text to create a 2D matrix
+// with the given inputs. Render the matrix on web page.
+// The given file must contain a text-based 2D matrix, where columns are
+// separated by commas and rows are separated by new lines.
+// Return: 2D matrix of characters.
+async function getBoard() {
+  const boardText = await readTextFile("/board.txt");
+  let board = [];
+  let row = 0,
+    column = 0;
+
+  // Populate the board (matrix) with the rows and columns in the file.
+  console.log(boardText);
+  boardText.split("\n").forEach((line) => {
+    if (line === "") return false; // check for last line
+
+    column = 0;
+    board[row] = [];
+
+    // Add each letter in the row to the board matrix.
+    line.split(",").forEach((letter) => {
+      board[row][column] = letter.toLowerCase();
+      column++;
+    });
+
+    row++;
+  });
+
+  // Render the given board on the web UI.
+  renderBoard(board);
+  return board;
+}
+
+// Given a filename, return the text in that file.
+async function readTextFile(fileName) {
+  // Use await for promise to settle.
+  const response = await fetch(fileName);
+  const myBlob = await response.blob();
+  const data = await myBlob.text();
+
+  return data; // string
+}
+
+// Given a 2D matrix, render the values onto the table in the document.
+function renderBoard(board) {
+  // creates a <table> element and a <tbody> element
+  let tableDiv = document.getElementById("col1");
+  let table = document.getElementById("boggleTable");
+  let trs = table.getElementsByTagName("tr");
+  let tds = null;
+
+  for (let i = 0; i < trs.length; i++) {
+    tds = trs[i].getElementsByTagName("td");
+    for (let j = 0; j < tds.length; j++) {
+      let cellStr = board[i][j].toUpperCase();
+      tds[j].innerText = cellStr;
+    }
+  }
+}
